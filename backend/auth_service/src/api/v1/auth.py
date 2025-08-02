95f22c77e2e00047f73c8a1a034f7fa2d777698e
“Vanya <coderyetanother@gmail.com>
2025-08-02 17:01:47 +0300

    feat: add the send code service to send code api endpoint.

--- backend/auth_service/src/api/v1/auth.py
+++ backend/auth_service/src/api/v1/auth.py
@@ -1,12 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, Depends
 
+from service.auth import (
+    SendCodeService,
+    get_send_code_service,
+)
+from schema.auth import SendCodeRequest
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post(path="/send_code")
-async def send_code():
-    pass
+async def send_code(
+    send_code_request: SendCodeRequest = Body(
+        description="Neccessary info to send code in telegram."
+    ),
+    send_code_service: SendCodeService = Depends(get_send_code_service),
+):
+    return await send_code_service.send_code(send_code_request=send_code_request)
+
 
 
 @router.post(path="/validate_code")
