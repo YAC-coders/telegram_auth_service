a4e634030c18427beba7574ba67108f630d53e33
VitalyMenkeev <login.nomad@ya.ru>
2025-08-02 15:59:43 +0300

    feat: q-form with q-input added

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,10 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <div class="q-pa-md" style="max-width: 400px">
+      <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="phoneNumber"
+        type="tel"
+        label="Your phone *"
+        hint="Enter your telephone number"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type phone number']"
+      />
+
+
+
+      <div>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
+    </div>
   </q-page>
 </template>
 
