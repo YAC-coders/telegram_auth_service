4fc6c13c9935e3c9a4c5193c42c05ea74c59b97e
VitalyMenkeev <login.nomad@ya.ru>
2025-08-02 17:06:51 +0300

    feat: IntlTelInput minimal component added to template

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,33 +1,33 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md" style="max-width: 400px">
-      <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-      <q-input
-        filled
-        v-model="phoneNumber"
-        type="tel"
-        label="Your phone *"
-        hint="Enter your telephone number"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type phone number']"
-      />
 
-
-
-      <div>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-        <q-btn label="Submit" type="submit" color="primary"/>
-      </div>
-    </q-form>
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <IntlTelInput v-model="phoneNumber" :options="{
+          initialCountry: 'ru',
+        }" />
+        <div>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
 
     </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import IntlTelInput from "intl-tel-input/vueWithUtils"
+import "intl-tel-input/styles"
+import { ref } from "vue"
+
+const phoneNumber = ref('')
+
+const onSubmit = () => {
+  console.log(phoneNumber.value)
+}
+
+const onReset = () => {
+  phoneNumber.value = ''
+}
 </script>
