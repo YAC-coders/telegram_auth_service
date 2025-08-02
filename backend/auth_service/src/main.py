0e0ee23dec04bd9f5ccc1fa527f7cd46af54c5d8
“Vanya <coderyetanother@gmail.com>
2025-08-02 14:47:53 +0300

    feat: define the fastapi application.

--- backend/auth_service/src/main.py
+++ backend/auth_service/src/main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from auth-service!")
+import logging
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from core.logger import LOGGING
+from core.settings import settings
+
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logging.info("Starup the application")
+    redis_storage.redis_storage = redis_storage.RedisStorage()
+    object_storage.object_storage = object_storage.ObjectStorage()
+    yield
+    logging.info("Stop the application")
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title=settings.project.title,
+    description=settings.project.description,
+    default_response_class=ORJSONResponse,
+)
+
+app.include_router(router=v1_router)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        app=app,
+        host=settings.uvcorn.host,
+        port=settings.uvcorn.port,
+        reload=False,
+        workers=settings.uvcorn.workers,
+        log_config=LOGGING,
+    )
