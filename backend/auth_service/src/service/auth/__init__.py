762c62ad50b01ca843518b22a53580958a364cdb
“Vanya <coderyetanother@gmail.com>
2025-08-03 15:06:42 +0300

    feat: create the validate code service.

--- backend/auth_service/src/service/auth/__init__.py
+++ backend/auth_service/src/service/auth/__init__.py
@@ -1,6 +1,8 @@
 from .send_code import SendCodeService, get_send_code_service
+from .validate_code import ValidateCodeService, get_validate_code_service
 
 __all__ = (
     "SendCodeService",
     "get_send_code_service",
+    "ValidateCodeService", "get_validate_code_service"
 )
