ccba954497cc9f6cb2565ce804d056641dd247fd
“Vanya <coderyetanother@gmail.com>
2025-08-14 14:17:45 +0700

    feat: create method to check record existence.

--- backend/auth_service/src/db/object/storage.py
+++ backend/auth_service/src/db/object/storage.py
@@ -10,13 +10,17 @@ class ObjectStorage:
         self.storage = {}
 
     def put_record(self, key: str, record: Any) -> None:
-        logging.info("Try to save data in object storage. Key: %s", key)
+        logging.info("Save data in object storage. Key: %s", key)
         self.storage.update({key: record})
         return None
 
     def get_record(self, key: str) -> Any | None:
-        logging.info("Try to retrieve data from object storage. Key: %s", key)
+        logging.info("Retrieve data from object storage. Key: %s", key)
         return self.storage.get(key)
+
+    def record_exists(self, key: str) -> bool:
+        logging.info("Check the record existence by key.")
+        return True if self.storage.get(key) else False
 
 
 object_storage: ObjectStorage | None = None
