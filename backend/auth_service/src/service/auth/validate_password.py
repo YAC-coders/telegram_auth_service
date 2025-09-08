e3f431ce1c12f61be66d0b2913434eb5fb5236c4
“Vanya <coderyetanother@gmail.com>
2025-09-08 18:28:52 +0300

    feat: add logic to handle exceptions to validate password service.

--- backend/auth_service/src/service/auth/validate_password.py
+++ backend/auth_service/src/service/auth/validate_password.py
@@ -1,10 +1,14 @@
+import datetime
 import logging
+from typing import Any
 
 from fastapi import Depends
+from telethon import TelegramClient
 
 from db.object.storage import ObjectStorage, get_object_storage
 from schema.auth import ValidatePasswordRequest, ValidatePasswordResponse
 from service.crypt import CryptRepository, get_crypt_repo
+from exception.telegram import PasswordExpired
 
 
 class ValidatePasswordService:
@@ -22,21 +26,58 @@ class ValidatePasswordService:
         """Retrieve client info from storage."""
         return self._object_storage.get_record(key=phone_number)
 
-    async def validate(self, validate_password_request: ValidatePasswordRequest):
-        phone_number = self._crypt_repo.decrypt(value=validate_password_request.session)
+    def _is_validation_expired(self, timestamp: datetime.datetime) -> bool:
+        """Check if validation period has expired."""
+        return datetime.datetime.now() - timestamp > datetime.timedelta(minutes=5)
 
-        client_info = await self._get_client_info(phone_number=phone_number)
-        if client_info:
-            client = client_info.get("client")
-            if client:
-                await client.sign_in(password=validate_password_request.password)
-                return ValidatePasswordResponse(
-                    session=validate_password_request.session
-                )
-        else:
-            logging.warning(
-                "Telegram auth time was expired. Account phone number: %s", phone_number
+    async def _handle_successful_validation(
+        self, client: TelegramClient, validate_password_request: ValidatePasswordRequest, phone_number: str, client_info: dict[str, Any]
+    ) -> ValidatePasswordResponse:
+        """Handle successful code validation."""
+        try:
+            await client.sign_in(password=validate_password_request.password)
+            self._object_storage.delete_record(key=phone_number)
+            return ValidatePasswordResponse(
+                session=validate_password_request.session, step="final"
             )
+        except Exception as exception:
+            logging.warning("Something went wrong when sign in via password. Error %s", str(exception))
+            raise
+
+    async def validate(self, validate_password_request: ValidatePasswordRequest) -> ValidatePasswordResponse:
+        phone_number = self._crypt_repo.decrypt(value=validate_password_request.session)
+        client_info = await self._get_client_info(phone_number=phone_number)
+
+        if not client_info:
+            logging.warning(
+                "Telegram connection was corrupted or was expired. Account phone number: %s",
+                phone_number,
+            )
+            return ValidatePasswordResponse(
+                session=validate_password_request.session, step="send_code"
+            )
+
+        current_step = client_info.get("step")
+        if current_step != "validate_code":
+            return ValidatePasswordResponse(
+                session=validate_password_request.session, step=current_step
+            )
+
+        if self._is_validation_expired(client_info.get("timestamp")):
+            logging.warning(
+                "Registration time was expired for phone: %s",
+                phone_number,
+            )
+            self._object_storage.delete_record(key=phone_number)
+            raise PasswordExpired("Registration time was expired for phone: %s")
+
+        return await self._handle_successful_validation(
+            client=client_info.get("client"),
+            phone_number=phone_number,
+            client_info=client_info,
+            validate_password_request=validate_password_request,
+        )
+
 
 
 def get_validate_password_service(
